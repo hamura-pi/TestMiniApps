1def49a321f550305979555ecfa00421c26d65a3
Andrei <andrei.seniorsam@gmail.com>
2019-09-24 16:35:09 +0300

    Updates

---
+++ precache-manifest.5fc17060b6ac759fd6ad89e15eb5e909.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "36857da3bd0eb89f1674",
+    "url": "./static/js/main.7c63ca3b.chunk.js"
+  },
+  {
+    "revision": "cdbd9032c7d3f475d9ab",
+    "url": "./static/js/2.e1c523a1.chunk.js"
+  },
+  {
+    "revision": "36857da3bd0eb89f1674",
+    "url": "./static/css/main.365c01a2.chunk.css"
+  },
+  {
+    "revision": "cdbd9032c7d3f475d9ab",
+    "url": "./static/css/2.0855360e.chunk.css"
+  },
+  {
+    "revision": "da1ce6c8bd24ff85815a5cad48c64e7a",
+    "url": "./index.html"
+  }
+];
